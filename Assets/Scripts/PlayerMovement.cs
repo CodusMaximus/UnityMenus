40277d740422f5a236293469472e70d3f3f1727a
Shaun <shaun@ivts.co.uk>
2020-12-08 22:14:29 +0000

    complete

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    public float Speed;
+
+    // Update is called once per frame
+    void Update() {
+
+        var x = Input.GetAxisRaw("Horizontal") * Speed;
+        var y = Input.GetAxisRaw("Vertical") * Speed;
+
+        transform.position += new Vector3(x, y);
+    }
+}
