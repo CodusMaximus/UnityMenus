3d3d140ff23788d9c5ede854a065e9f50f8f642a
Shaun <shaun@ivts.co.uk>
2020-12-20 10:55:09 +0000

    Networking game complete

---
+++ Assets/Scripts/NetworkGameSetupController.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class NetworkGameSetupController : MonoBehaviour {
+    void Start() {
+        CreatePlayer();
+    }
+
+    private void CreatePlayer() {
+        Debug.Log("Creating Player");
+        if (PhotonNetwork.IsMasterClient) {
+            PhotonNetwork.Instantiate("Player", Vector3.zero - new Vector3(0, 0, 1), Quaternion.identity);
+        }
+        else {
+            PhotonNetwork.Instantiate("Player", Vector3.zero - new Vector3(0, 0, 1), Quaternion.identity);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,21 @@
-﻿using UnityEngine;
+﻿using Photon.Pun;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
     [SerializeField]
     public float Speed;
 
+    private PhotonView myView;
+    private void Start() {
+        myView = GetComponent<PhotonView>();
+    }
+
     // Update is called once per frame
     void Update() {
+        if (!myView.IsMine && PhotonNetwork.IsConnected) {
+            return;
+        }
 
         var x = Input.GetAxisRaw("Horizontal") * Speed;
         var y = Input.GetAxisRaw("Vertical") * Speed;
